4dd62a72df04c13fc11786573889023de1c6b17f
Marvin <aguirreestuardo758@gmail.com>
2023-09-29 21:19:32 -0600

    Se agregaron archivos

---
+++ Clase8/config/sql_server.js
@@ -0,0 +1,12 @@
+
+const config = {
+    server: "localhost\\EXAMPLE_SQL",
+    user: "sa",
+    password:"1234",
+    database: "Alumnos",
+    options: {
+        trustServerCertificate: true
+    }
+}
+
+module.exports.config = config;
